a233fe048b6beb7acb984357c81e2bdd921d9bfa
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-15 17:36:17 +0900

    propsの渡し方

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>{{ user }}さんこんにちは</h1>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -11,8 +12,19 @@ import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      user: "",
+    }
+  },
   components: {
     HelloWorld,
+  },
+  // props: ["user"],
+  async created() {
+    this.user = await this.$route.query.user
+
+    console.log(this.user)
   },
 }
 </script>
