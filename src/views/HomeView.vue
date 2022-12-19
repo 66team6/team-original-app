e90f043aef63afe778f46cfc3de2630af3c52d57
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-19 22:32:40 +0900

    修正の完了

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>{{ user }}さんこんにちは</h1>
+  <button @click="goToLogin">ログインへ</button>
+  <!-- <h1>{{ user }}</h1> -->
+  <h1>{{ message }}</h1>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <ShowViewVue></ShowViewVue>
@@ -9,6 +11,7 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue"
+// import loginViewVue from "./loginView.vue"
 import ShowViewVue from "./ShowView.vue"
 export default {
   name: "HomeView",
@@ -16,6 +19,14 @@ export default {
     return {
       user: "",
     }
+  },
+  methods: {
+    getName(value) {
+      this.user = value
+    },
+    goToLogin() {
+      this.$router.push({ name: "login" })
+    },
   },
   components: {
     ShowViewVue,
@@ -25,6 +36,15 @@ export default {
     this.user = await this.$route.query.user
 
     console.log(this.user)
+  },
+  computed: {
+    message() {
+      if (this.user.length) {
+        return this.user + "さんこんにちは"
+      } else {
+        return "ログインしてください"
+      }
+    },
   },
 }
 </script>
--- src/views/ShowView.vue
+++ src/views/ShowView.vue
@@ -20,6 +20,7 @@ export default {
   components: {
     MakeCommunityVue,
   },
+  props: ["user"],
   data() {
     return {
       communities: [],
