87387f9ebd3a0bedf881dd68e91a962cc8aeebd2
kanna1204 <masegiganko@gmail.comgit config --global user.name kanna1204>
2022-12-21 18:16:41 +0900

    login画面のデザインほぼ終わった

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -1,21 +1,32 @@
 <template>
   <header>
+    <div class="appphoto"></div>
     <div class="appname">Beacon</div>
   </header>
   <main>
     <div class="photo"></div>
     <div class="login">
-      <section id="v-if-sign-in" class="signin">
+      <div class="loginicon"></div>
+      <div class="loginfont">ログイン</div>
+      <section id="v-if-sign-in" class="loginbutton">
         <div v-if="user !== null">
-          <button v-on:click="signOut">サインアウト</button>
-          <div>{{ user.displayName }}さん、ログイン完了です。</div>
+          <button class="btn btn--yellow btn--cubic" v-on:click="signOut">
+            Sign Out
+          </button>
+          <div class="direction">
+            {{ user.displayName }}さん、ログイン完了です。
+          </div>
         </div>
         <div v-else>
-          <button v-on:click="signIn">サインイン</button>
-          <div>サインインしてください。</div>
+          <button class="btn btn--yellow btn--cubic" v-on:click="signIn">
+            サインイン
+          </button>
+          <div class="direction">サインインしてください。</div>
         </div>
       </section>
-      <button @click="goHomePage">ホームへ戻る</button>
+      <button class="btn btn--yellow2 btn--cubic" @click="goHomePage">
+        ホームへ
+      </button>
       <div id="profile"></div>
     </div>
   </main>
@@ -23,6 +34,8 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Caveat&display=swap");
+
 * {
   margin: 0;
   padding: 0;
@@ -31,35 +44,116 @@
 header {
   width: 100%;
   height: 100px;
-  left: 0px;
-  top: 0px;
+  display: flex;
+  justify-content: flex-start;
+  padding-top: 10px;
+  padding-left: 30px;
+  /* left: 0px;
+  top: 0px; */
   /* background-color: aqua; */
+}
+.appphoto {
+  background-image: url(C:\Users\maseg\Desktop\webex\team-original-app\Component_17.png);
+  background-size: contain;
+  width: 80px;
+  height: 80px;
+  margin-right: 20px;
+}
+.appname {
+  font-size: 70px;
+  font-family: "Caveat", cursive;
+  /* padding-top: px; */
 }
 main {
   width: 100%;
   height: 410px;
-  background: rgba(217, 217, 217, 0.4);
+  background: #f1f5f7;
   display: flex;
   /* justify-content: center; */
 }
 .photo {
-  margin: 20px;
-  width: 55%;
+  margin: 30px;
+  width: 40%;
   height: 95%;
-  background-image: url(C:\Users\maseg\Desktop\webex\team-original-app\1.jpg);
-  background-size: cover;
+  background-image: url(C:\Users\maseg\Desktop\webex\team-original-app\Rectangle_54.png);
+  background-size: contain;
   border-radius: 15px;
+
   /* display: inline-block; */
 }
 .login {
   /* display: inline-block; */
-  margin: 50px;
+  padding-top: 30px;
+  padding-left: 200px;
+  padding-right: 221px;
+  /* border-top: 30px solid #ffffff; */
+  border-right: 30px solid #ffffff;
+  border-left: 30px solid #ffffff;
+}
+.loginicon {
+  background-image: url(C:\Users\maseg\Desktop\webex\team-original-app\ic_baseline-person-pin.png);
+  width: 38%;
+  height: 19%;
+  background-size: cover;
+  margin-left: 70px;
+}
+.loginfont {
+  font-size: 30px;
+  font-weight: bold;
+  font-family: "Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体";
+}
+.loginbutton {
+  padding-top: 40px;
+  font-weight: bold;
+
+  font-family: "Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体";
+}
+.btn--yellow {
+  color: #000;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  background-color: #ffff88;
+  border-bottom: 5px solid #ccc100;
+  border-radius: 10px;
+  font-weight: bold;
+}
+.direction {
+  margin-top: 15px;
+  font-family: "Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体";
+  font-size: large;
+}
+.btn--yellow2 {
+  margin-top: 15px;
+  font-weight: bold;
+
+  width: 100px;
+  height: 40px;
+  color: #000;
+  font-size: 14px;
+  background-color: #ffff88;
+  border-radius: 10px;
+  border-bottom: 5px solid #ccc100;
 }
 
+.btn--yellow:hover {
+  margin-top: 3px;
+  color: #000;
+  background: #ffff88;
+  border-bottom: 2px solid #ccc100;
+}
+.btn--yellow2:hover {
+  margin-top: 3px;
+  color: #000;
+  background: #ffff88;
+  border-bottom: 2px solid #ccc100;
+}
 footer {
   width: 100%;
   height: 43px;
-  background-color: blueviolet;
+  background-color: #ffff88;
+  padding-top: 14px;
+  /* font-family: "Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体"; */
 }
 </style>
 
